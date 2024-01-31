c00c5fd99b8dfdd58835b0072d9c6f773f71f500
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2024-01-31 17:17:24 -0300

    funcionalidade adicionar e tirar itens

--- js/app.js
+++ js/app.js
@@ -87,9 +87,51 @@ cardapio.metodos = {
           MEU_CARRINHO.push(item[0]);
         }
 
+        cardapio.metodos.mensagem("Item adicionado ao carrinho", "green");
         $("#qntd-" + id).text(0);
+
+        cardapio.metodos.atualizarBadgeTotal();
       }
     }
+  },
+
+  atualizarBadgeTotal: () => {
+    var total = 0;
+    $.each(MEU_CARRINHO, (i, e) => {
+      total += e.qntd;
+    });
+
+    if (total > 0) {
+      $(".botao-carrinho").removeClass("hidden");
+      $(".container-total-carrinho").removeClass("hidden");
+    } else {
+      $(".botao-carrinho").addClass("hidden");
+      $(".container-total-carrinho").addClass("hidden");
+    }
+    $(".badge-total-carrinho").html(total);
+  },
+
+  abrirCarrinho: (abrir) => {
+    if (abrir) {
+      $("#modalCarrinho").removeClass("hidden");
+    } else {
+      $("#modalCarrinho").addClass("hidden");
+    }
+  },
+
+  mensagem: (texto, cor = "red", tempo = 2500) => {
+    let id = Math.floor(Date.now() * Math.random()).toString();
+
+    let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+    $("#container-mensagens").append(msg);
+
+    setTimeout(() => {
+      $("#msg-" + id).removeClass("fadeInDown");
+      $("#msg-" + id).addClass("fadeOutUp");
+      setTimeout(() => {
+        $("#msg-" + id).remove();
+      }, 800);
+    }, tempo);
   },
 };
 
