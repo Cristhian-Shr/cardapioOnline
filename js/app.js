07752052d9fdd6c83944bdd2da9f25d57520b3a9
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2024-02-06 21:54:23 -0300

    add funcionalidades dos btns do modal o cardapio

--- js/app.js
+++ js/app.js
@@ -114,9 +114,63 @@ cardapio.metodos = {
   abrirCarrinho: (abrir) => {
     if (abrir) {
       $("#modalCarrinho").removeClass("hidden");
+      cardapio.metodos.carregarEtapa(1);
     } else {
       $("#modalCarrinho").addClass("hidden");
     }
+  },
+
+  carregarEtapa: (etapa) => {
+    if (etapa == 1) {
+      $("#lblTituloEtapa").text("Seu carrinho:");
+      $("#itensCarrinho").removeClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumoCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+
+      $("#btnEtapaPedido").removeClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").addClass("hidden");
+    }
+    if (etapa == 2) {
+      $("#lblTituloEtapa").text("Endereço de entrega:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").removeClass("hidden");
+      $("#resumoCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").removeClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+    if (etapa == 3) {
+      $("#lblTituloEtapa").text("Resumo do pedido:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumoCarrinho").removeClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+      $(".etapa3").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").removeClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+  },
+
+  voltarEtapa: () => {
+    let etapa = $(".etapa.active").length;
+    cardapio.metodos.carregarEtapa(etapa - 1);
   },
 
   mensagem: (texto, cor = "red", tempo = 2500) => {
