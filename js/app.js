02d497ff6d69d951c0ac9e3a0b1025b79d8d6028
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2024-02-09 15:24:16 -0300

    manipulando valores do carrinho

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ var cardapio = {};
 
 var MEU_CARRINHO = [];
 
+var VALOR_CARRINHO = 0;
+
+var VALOR_ENTREGA = 5;
+
 cardapio.eventos = {
   init: () => {
     cardapio.metodos.obterItensCardapio();
@@ -188,11 +192,16 @@ cardapio.metodos = {
           .replace(/\${qntd}/g, e.qntd);
 
         $("#itensCarrinho").append(temp);
+
+        if (i + 1 == MEU_CARRINHO.length) {
+          cardapio.metodos.carregarValores();
+        }
       });
     } else {
       $("#itensCarrinho").html(
         "<p class='carrinho-vazio'><i class='fa fa-shopping-bag'></i> Sua sacola esta vazia!</p>"
       );
+      cardapio.metodos.carregarValores();
     }
   },
 
@@ -233,6 +242,32 @@ cardapio.metodos = {
     MEU_CARRINHO[objIndex].qntd = qntd;
 
     cardapio.metodos.atualizarBadgeTotal();
+
+    cardapio.metodos.carregarValores();
+  },
+
+  carregarValores: () => {
+    VALOR_CARRINHO = 0;
+
+    $("#lblSubTotal").text("R$ 0,00");
+    $("#lblValorEntrega").text("+ R$ 0,00");
+    $("#lblValorTotal").text("R$ 0,00");
+
+    $.each(MEU_CARRINHO, (i, e) => {
+      VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+      if (i + 1 == MEU_CARRINHO.length) {
+        $("#lblSubTotal").text(
+          `R$ ${VALOR_CARRINHO.toFixed(2).replace(".", ",")}`
+        );
+        $("#lblValorEntrega").text(
+          `+ R$ ${VALOR_ENTREGA.toFixed(2).replace(".", ",")}`
+        );
+        $("#lblValorTotal").text(
+          `R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace(".", ",")}`
+        );
+      }
+    });
   },
 
   mensagem: (texto, cor = "red", tempo = 2500) => {
